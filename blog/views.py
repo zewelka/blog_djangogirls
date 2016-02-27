ce66ece383d1e34073bfa9bed7cc43dbc88f6026
zewelka <zewelka@gazeta.pl>
2016-02-27 16:28:14 +0100

    Zmieniony kod HTML na stronie.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. View - funkcja ta sie wykona gdy wejdziemy pod adres mojego bloga
+def post_list(request):# request - zbiór danych z info o zapytaniu
+    return render(request, 'blog/post_list.html', {}) # wywołanie funkcji render; wygenerowanie (wyrenderowanie) odpowiedzi dla przegladarki
